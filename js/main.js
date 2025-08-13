e5afe84122feeb49a9c31e408a2dba9fffc90194
jeonyw85 <loveisleejin@naver.com>
2025-08-13 09:34:05 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
 
-searchEl.addEventListener('click', function () {
-    //Logic 입력
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl = document.querySelector('header .badges');
 
@@ -153,5 +137,3 @@ spyEls.forEach(function (spyEl) {
         .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
